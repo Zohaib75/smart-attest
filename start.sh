0b4337a990be8de8a0b72fd32739abefcd742570
zohaib <zohaib.butt75@ucp.edu.pk>
2018-12-14 02:26:05 +0500

    third commit

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-composer network install --card PeerAdmin@hlfv1 --archiveFile smart-attest.bna
-composer network start --networkName smart-attest --networkVersion 0.0.3-deploy.7 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
+composer network install --card PeerAdmin@hlfv1 --archiveFile smart-attest@0.0.3-deploy.60.bna
+composer network start --networkName smart-attest --networkVersion 0.0.3-deploy.60 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
 composer card import --file networkadmin.card
 composer network ping --card admin@smart-attest
